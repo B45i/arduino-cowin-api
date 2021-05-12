ed62e2712f5f50394ffe3c05df2161634f7b527c
b45i <amalshajan2011@gmail.com>
2021-05-12 10:26:04 +0530

    new UA

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.options('*', cors());
 
 const sampleUserAgent =
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36';
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36';
 
 const port = process.env.PORT || 5000;
 
